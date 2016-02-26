ad8132c1e0fae0c2115f00d8b755083354011ad0
Travis Augustine <augustine@ku.edu>
2016-02-26 10:32:45 -0600

    Add Quiz php file

---
+++ Quiz.php
@@ -0,0 +1,9 @@
+<?php
+//access the global array called $_POST to get the values from the text fields
+$name = $_POST["name"];
+$email = $_POST["email"];
+
+echo "Name: " . $name . "<br>";
+echo "Email: " . $email . "<br>";
+
+?>
