7b05262f09de9a02fda2af60fe31a57e92ca3af4
Travis Augustine <augustine@ku.edu>
2016-02-27 12:21:10 -0600

    Update verification

--- formChecker.js
+++ formChecker.js
@@ -1,21 +1,21 @@
 function validateForm() {
 
 	var x = document.forms["cart"]["cool"].value;
-	if (x == null || x == "")
+	if (x == null || x == "" || x <0)
 	{
 		alert("Item quantities cannot be blank or negative");
 		return false;
 	}
 
 	var y = document.forms["cart"]["shades"].value;
-	if (y == null || y == "")
+	if (y == null || y == "" || y<0)
 	{
 		alert("Item quantities cannot be blank or negative");
 		return false;
 	}
 
 	var z = document.forms["cart"]["boss"].value;
-	if (z == null || z == "")
+	if (z == null || z == "" || z<0)
 	{
 		alert("Item quantities cannot be blank or negative");
 		return false;
@@ -27,6 +27,20 @@ function validateForm() {
 	if(!three && !overnight && !free)
 	{
 		alert("A shipping option must be selected");
+		return false;
+	}
+
+	var username = document.forms["cart"]["username"].value;
+	if(username == null || username == "")
+	{
+		alert("Username cannot be blank");
+		return false;
+	}
+
+	var password = document.forms["cart"]["password"].value;
+	if(password == null || password == "")
+	{
+		alert("Password cannot be blank");
 		return false;
 	}
 
