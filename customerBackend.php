e4021b1178a5cba6b2c9a52e78a18a75f570feba
Travis Augustine <augustine@ku.edu>
2016-02-27 11:21:01 -0600

    Fix html table for all outputs

--- customerBackend.php
+++ customerBackend.php
@@ -1,5 +1,4 @@
 <?php
-
 $cool = $_POST["cool"];
 $shades = $_POST["shades"];
 $boss = $_POST["boss"];
@@ -9,7 +8,7 @@ $password = $_POST["password"];
 
 $coolSub = 20*$cool;
 $shadesSub = 35*$shades;
-$bossSub = 15*$boss
+$bossSub = 15*$boss;
 $shippingSub = 0;
 
 if($shipping == "overnight")
@@ -27,17 +26,20 @@ else if($shipping == "three")
 }
 
 
+$totalCost = $coolSub + $shadesSub + $bossSub + $shippingSub;
 
 echo '<table>';
 echo '<tr><td></td><td>Quantity</td><td>Cost Per Item</td><td>Sub Total</td></tr>';
 
 echo '<tr><td>Cool Hat</td><td>'.$cool.'</td><td>$20.00</td><td>'.$coolSub.'</td></tr>';
 
-echo '<tr><td>Cool Hat</td><td>'.$shades.'</td><td>$20.00</td><td>'.$shadesSub.'</td></tr>';
+echo '<tr><td>Shades Hat</td><td>'.$shades.'</td><td>$35.00</td><td>'.$shadesSub.'</td></tr>';
 
-echo '<tr><td>Cool Hat</td><td>'.$boss.'</td><td>$20.00</td><td>'.$bossSub.'</td></tr>';
+echo '<tr><td>Boss Hat</td><td>'.$boss.'</td><td>$15.00</td><td>'.$bossSub.'</td></tr>';
 
 echo '<tr><td>Shipping</td><td colspan = 2>'.$shipping.'</td><td>'.$shippingSub.'</td></tr>';
+
+echo '<tr><td colspan = 3>Total Cost</td><td>'.$totalCost.'</td></tr>';
 
 echo '</table>';
 
